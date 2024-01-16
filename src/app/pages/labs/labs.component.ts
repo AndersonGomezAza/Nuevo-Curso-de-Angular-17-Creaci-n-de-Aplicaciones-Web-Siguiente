4073320fc8d34e9ed025442df10e346516bf07a3
unknown <argomeza@itc.edu.co>
2024-01-16 11:01:32 -0500

    Uploading class mostrando elementos

---
+++ src/app/pages/labs/labs.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
+})
+export class LabsComponent {
+  welcome = 'Bienvenido a mi primera aplicacion de angular';
+  tasks = [
+    'Instalar Angular CLI',
+    'Crear Proyecto',
+    'Crear Componente',
+    'Crear Servicio'
+  ];
+}
